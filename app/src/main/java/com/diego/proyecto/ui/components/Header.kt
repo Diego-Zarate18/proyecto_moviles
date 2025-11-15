f0dd6a5c3dcc8f9dcfbbba4176728a7948b46d54
DAXTER1829 <diegoazarate110800@gmail.com>
2025-11-15 14:45:51 -0600

    header

--- app/src/main/java/com/diego/proyecto/ui/components/Header.kt
+++ app/src/main/java/com/diego/proyecto/ui/components/Header.kt
@@ -1,2 +1,33 @@
 package com.diego.proyecto.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.diego.proyecto.R
+
+@Composable
+fun Header() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_b),
+            contentDescription = "Musio Logo",
+            modifier = Modifier.height(30.dp)
+        )
+
+    }
+}
